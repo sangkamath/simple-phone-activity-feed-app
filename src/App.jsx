0542e37b4826f7fb2a8df6508061d561778ecebc
sangkamath <twinkle_kamath@yahoo.com>
2025-03-08 17:50:44 -0800

    Added footer and logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import { createRoot } from 'react-dom/client';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { FaPhoneAlt } from "react-icons/fa";
-import { IconContext } from "react-icons";
 const BASE_URL = 'https://aircall-api.onrender.com';
 import { CiCircleInfo } from "react-icons/ci";
 import { RiInboxArchiveLine } from "react-icons/ri";
 import { RiInboxUnarchiveLine } from "react-icons/ri";
+import { FaCircleUser } from "react-icons/fa6";
+import { MdDialpad } from "react-icons/md";
+import { IoIosSettings } from "react-icons/io";
+import { FaVoicemail } from "react-icons/fa";
+
+
 
 // Helper function to group calls by date (YYYY-MM-DD) and sort each group by time.
 const groupCallsByDate = (calls) => {
   // Sort the calls in ascending order of creation time.
   const sortedCalls = [...calls].sort(
-    (a, b) => new Date(a.created_at) - new Date(b.created_at)
+    (a, b) => new Date(b.created_at) - new Date(a.created_at)
   );
   // Group by date.
   return sortedCalls.reduce((groups, call) => {
@@ -27,8 +32,18 @@ const groupCallsByDate = (calls) => {
   }, {});
 };
 
+const formatDate = (date) => {
+  const d = new Date(date);
+  const formattedDate = d.toLocaleDateString('en-US', {
+    timeZone: 'UTC',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  });
+  return formattedDate;
+}
+
 const App = () => {
-  const [activities, setActivities] = useState([]);  // State to store fetched activities
   const [loading, setLoading] = useState(true);       // Loading state to show while fetching data
   // 'feed' or 'archived' or 'detail'
   const [activeTab, setActiveTab] = useState('feed');
@@ -160,8 +175,9 @@ const App = () => {
     <div className='container'>
       <header className="header">
         <div className="header__left">
-          <span className="button-square">
-           </span>
+          <div className='logo'>
+            <img src="./public/logo.png" alt="Logo" className="logo__image" />
+          </div>
         </div>
         <div className="header__right">
           <nav className="nav">
@@ -188,7 +204,7 @@ const App = () => {
           </nav>
         </div>
       </header >
-      <div className="app-container">
+      <div className="container-view">
 
 
         {/* Content Area */}
@@ -204,7 +220,7 @@ const App = () => {
               ) : (
                 Object.keys(groupedData).map((date) => (
                   <div key={date} className="date-group">
-                    <h3>{date}</h3>
+                    <h3>{formatDate(date)}</h3>
                     <ul className="call-list">
                       {groupedData[date].map((call) => (
                         <li key={call.id} className="call-item">
@@ -213,7 +229,9 @@ const App = () => {
                               <strong>{call.direction}</strong> call from {call.from} to {call.to} ({call.call_type})
                             </p>
                             <p>
-                              {new Date(call.created_at).toLocaleTimeString()}
+                              {new Date(call.created_at).toLocaleTimeString('en-US', {
+                                timeZone: 'UTC'
+                              })}
                             </p>
                           </div>
                           <div className="actions">
@@ -221,11 +239,11 @@ const App = () => {
                               className="button-square"
                               onClick={() => viewCallDetail(call)}
                             >
-                             <CiCircleInfo size={25}/>
+                              <CiCircleInfo size={25} />
                             </button>
-                            <button  className="button-square"
-                            onClick={() => archiveCall(call.id)}>
-                              <RiInboxArchiveLine size={25}/>
+                            <button className="button-square"
+                              onClick={() => archiveCall(call.id)}>
+                              <RiInboxArchiveLine size={25} />
                             </button>
                           </div>
                         </li>
@@ -248,7 +266,7 @@ const App = () => {
               ) : (
                 Object.keys(groupedData).map((date) => (
                   <div key={date} className="date-group">
-                    <h3>{date}</h3>
+                    <h3>{formatDate(date)}</h3>
                     <ul className="call-list">
                       {groupedData[date].map((call) => (
                         <li key={call.id} className="call-item">
@@ -257,19 +275,21 @@ const App = () => {
                               <strong>{call.direction}</strong> call from {call.from} to {call.to} ({call.call_type})
                             </p>
                             <p>
-                              {new Date(call.created_at).toLocaleTimeString()}
+                              {new Date(call.created_at).toLocaleTimeString('en-US', {
+                                timeZone: 'UTC'
+                              })}
                             </p>
                           </div>
                           <div className="actions">
-                          <button
+                            <button
                               className="button-square"
                               onClick={() => viewCallDetail(call)}
                             >
-                             <CiCircleInfo size={25}/>
+                              <CiCircleInfo size={25} />
                             </button>
                             <button className="button-square" onClick={() => unarchiveCall(call.id)}>
-                            <RiInboxUnarchiveLine size={25} />
-                              </button>
+                              <RiInboxUnarchiveLine size={25} />
+                            </button>
                           </div>
                         </li>
                       ))}
@@ -291,7 +311,9 @@ const App = () => {
               <p><strong>To:</strong> {selectedCall.to}</p>
               <p><strong>Call Type:</strong> {selectedCall.call_type}</p>
               <p><strong>Duration:</strong> {selectedCall.duration} seconds</p>
-              <p><strong>Created At:</strong> {new Date(selectedCall.created_at).toLocaleString()}</p>
+              <p><strong>Created At:</strong> {new Date(selectedCall.created_at).toLocaleString('en-US', {
+                timeZone: 'UTC'
+              })}</p>
               <p>
                 <strong>Archived:</strong>{' '}
                 {selectedCall.is_archived ? 'Yes' : 'No'}
@@ -299,17 +321,59 @@ const App = () => {
               <br />
               {selectedCall.is_archived ? (
                 <button className="button-square" onClick={() => unarchiveCallFromDetails(selectedCall.id)}>
-                  <RiInboxArchiveLine size={25}/> Unarchive
+                  <RiInboxArchiveLine size={25} /> Unarchive
                 </button>
               ) : (
-                <button  className="button-square" onClick={() => archiveCallFromDetails(selectedCall.id)}>
-                  <RiInboxUnarchiveLine size={25}/> Archive
+                <button className="button-square" onClick={() => archiveCallFromDetails(selectedCall.id)}>
+                  <RiInboxUnarchiveLine size={25} /> Archive
                 </button>
               )}
             </div>
           )}
         </div>
       </div>
+
+      <footer className="footer">
+        <div className="footer__content">
+          <ul className="nav__list footer__list">
+          <span
+                className={`nav__item  ${activeTab === 'feed' ? 'active' : ''}`}
+              >
+                <button className="button-square" >
+                  <FaPhoneAlt size={20} color="green"/> 
+                </button>
+              </span>
+              <span
+                className={`nav__item ${activeTab === 'archived' ? 'active' : ''}`}
+              >
+                <button className="button-square">
+                  <FaCircleUser size={25} /> 
+                </button>
+              </span>
+              <span
+                className={`nav__item ${activeTab === 'archived' ? 'active' : ''}`}
+              >
+                <button className="button-square" >
+                  <MdDialpad size={25} /> 
+                </button>
+              </span>
+              <span
+                className={`nav__item ${activeTab === 'archived' ? 'active' : ''}`}
+              >
+                <button className="button-square" >
+                <IoIosSettings size={25} />
+                </button>
+              </span>
+              <span
+                className={`nav__item ${activeTab === 'archived' ? 'active' : ''}`}
+              >
+                <button className="button-square" >
+                <FaVoicemail size={25} /> 
+                  </button>
+              </span>
+          </ul>
+        </div>
+      </footer>
     </div >
   );
 };
